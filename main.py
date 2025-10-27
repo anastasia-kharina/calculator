4b3a7dab46b22df5ca8bbaa9e4d2edc2a2a3bc90
User <kosaktigr8@gmail.com>
2025-10-27 22:05:17 +0300

    Реализованы вычисления

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("Введите данные")
 num1, num2, znak = int(input()), int(input()), input()
+if znak == '+':
+    print(num1+num2)
+elif znak == '-':
+    print(num1-num2)
+elif znak == '*':
+    print(num1*num2)
+elif znak == '/':
+    print(num1/num2)
+else:
+    print("Неверные данные")
